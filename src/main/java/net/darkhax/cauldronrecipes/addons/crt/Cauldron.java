850377eb09e765510397e5598f5d81ee621b90f6
Tyler Hancock <darklime@live.ca>
2020-07-21 16:49:15 -0600

    Refactored CraftTweaker support to better use existing CrT code.

--- src/main/java/net/darkhax/cauldronrecipes/addons/crt/Cauldron.java
+++ src/main/java/net/darkhax/cauldronrecipes/addons/crt/Cauldron.java
@@ -2,13 +2,17 @@ package net.darkhax.cauldronrecipes.addons.crt;
 
 import org.openzen.zencode.java.ZenCodeType;
 
+import com.blamejared.crafttweaker.CraftTweaker;
 import com.blamejared.crafttweaker.api.CraftTweakerAPI;
 import com.blamejared.crafttweaker.api.annotations.ZenRegister;
 import com.blamejared.crafttweaker.api.item.IIngredient;
 import com.blamejared.crafttweaker.api.item.IItemStack;
 import com.blamejared.crafttweaker.api.managers.IRecipeManager;
+import com.blamejared.crafttweaker.impl.actions.recipes.ActionAddRecipe;
+import com.blamejared.crafttweaker.impl.helper.CraftTweakerHelper;
 
 import net.darkhax.cauldronrecipes.CauldronRecipes;
+import net.darkhax.cauldronrecipes.RecipeCauldron;
 import net.minecraft.item.crafting.IRecipeType;
 import net.minecraft.util.ResourceLocation;
 
@@ -25,25 +29,28 @@ public class Cauldron implements IRecipeManager {
     @ZenCodeType.Method
     public void addRecipe (String id, IIngredient input, IItemStack outputs) {
         
-        CraftTweakerAPI.apply(new ActionCreateRecipe(id, input, 1, outputs));
+        this.addRecipe(id, input, 1, outputs);
     }
     
     @ZenCodeType.Method
     public void addRecipe (String id, IIngredient input, int fluidLevel, IItemStack outputs) {
         
-        CraftTweakerAPI.apply(new ActionCreateRecipe(id, input, fluidLevel, outputs));
+        this.addRecipe(id, input, fluidLevel, new IItemStack[] { outputs });
     }
     
     @ZenCodeType.Method
     public void addRecipe (String id, IIngredient input, IItemStack[] outputs) {
         
-        CraftTweakerAPI.apply(new ActionCreateRecipe(id, input, 1, outputs));
+        this.addRecipe(id, input, 1, outputs);
     }
     
     @ZenCodeType.Method
     public void addRecipe (String id, IIngredient input, int fluidLevel, IItemStack[] outputs) {
         
-        CraftTweakerAPI.apply(new ActionCreateRecipe(id, input, fluidLevel, outputs));
+        id = this.validateRecipeName(id);
+        final ResourceLocation recipeId = new ResourceLocation(CraftTweaker.MODID, id);
+        final RecipeCauldron recipe = new RecipeCauldron(recipeId, input.asVanillaIngredient(), fluidLevel, CraftTweakerHelper.getItemStacks(outputs));
+        CraftTweakerAPI.apply(new ActionAddRecipe(this, recipe, ""));
     }
     
     @Override
@@ -53,7 +60,7 @@ public class Cauldron implements IRecipeManager {
     }
     
     @Override
-    public IRecipeType getRecipeType () {
+    public IRecipeType<?> getRecipeType () {
         
         return CauldronRecipes.recipeType;
     }
